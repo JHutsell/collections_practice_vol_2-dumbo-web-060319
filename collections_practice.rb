882e89dc92f60052438be1de6fae2126705d24d6
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 23:23:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -67,7 +67,9 @@ def organize_schools(schools_hash)
   organized_hash = {}
   
   schools_hash.map do |name, location|
-    location.map do |
+    location.map do |key, city|
+      
+    end
   end
   
   
